70f9f5cf6f22fd92e1debe6f879c932cc782510a
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-02-24 22:20:34 +0200

    исправлены 1-е и 2-е задания

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,8 @@
-`use strict`;
-
-function makeTransaction (quantity, pricePerDroid) {
+function makeTransaction(quantity, pricePerDroid) {
     let totalPrice = quantity * pricePerDroid;
-    return {totalPrice, quantity};
+    return "You ordered " + quantity + " droids worth " + totalPrice + " credits!";
 }
 
-let transaction = makeTransaction (5, 3000);
-
-console.log("You ordered " + transaction.quantity + " droids worth " + transaction.totalPrice + " credits!");
-/*
-console.log(makeTransaction(5, 3000));*/
-
-transaction = makeTransaction (3, 1000);
-/*
-console.log(makeTransaction(3, 1000));*/
-
-console.log("You ordered " + transaction.quantity + " droids worth " + transaction.totalPrice + " credits!");
-
-transaction = makeTransaction (10, 500);
-
-console.log("You ordered " + transaction.quantity + " droids worth " + transaction.totalPrice + " credits!");
-/*
-console.log(makeTransaction(10, 500));*/
-
-/*
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
-*/
